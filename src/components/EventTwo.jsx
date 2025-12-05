71bfa20ed8074fc61d8825d0bb46b2032749cccb
farhan8765 <farhan@aimarklabs.com>
2025-12-05 10:59:43 +0500

    data updated

---
+++ src/components/EventTwo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import EventSummit from './EventSummit';
+import EventMap from './Eventmap';
+
+const EventTwo = () => {
+  return (
+    <div>
+      <EventSummit />
+      <EventMap />
+    </div>
+  );
+};
+
+export default EventTwo;
+
